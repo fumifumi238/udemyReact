2d11d335a0d4b019750dcd37d22dcbffa94b9550
epthalite <fmy628.1998@icloud.com>
2021-11-18 21:32:53 +0900

    increment events

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import {createStore} from 'redux'
-import { Provider } from 'react-redux';
-import App from './components/App';
-import reducer from './reducers'
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk"
 
-const store = createStore(reducer);
+import EventsIndex from "./components/events_index";
+import reducer from "./reducers";
+import reportWebVitals from "./reportWebVitals";
+
+const store = createStore(reducer,applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <EventsIndex />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuring performance in your EventsIndex, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
